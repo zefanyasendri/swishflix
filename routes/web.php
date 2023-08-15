fc0d67d31fc18753252e303a90ec4961650ca7bf
zefanyasendri <zefanyasendri@gmail.com>
2023-08-16 01:47:12 +0700

    feat: Setup Spatie

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,14 @@ use Inertia\Inertia;
 |
 */
 
+// Route::get('admin', function(){
+//     return 'Hi Admin';
+// })-> middleware('role:admin');
+
+// Route::get('user', function(){
+//     return 'Hi User';
+// })-> middleware('role:user');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
