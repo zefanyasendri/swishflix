ceaf1ad329cf8bd7c901db635b7459b31b525f68
zefanyasendri <zefanyasendri@gmail.com>
2023-08-21 18:42:59 +0700

    feat: Slicing Dashboard

--- resources/js/Pages/Prototype/Register.jsx
+++ resources/js/Pages/Prototype/Register.jsx
@@ -62,11 +62,13 @@ export default function Register() {
                                 </div>
                             </div>
                             <div className="grid space-y-[14px] mt-[30px]">
-                                <PrimaryButton>
-                                    <span className="text-base font-semibold">
-                                        Sign Up
-                                    </span>
-                                </PrimaryButton>
+                                <Link href={route('prototype.login')}>
+                                    <PrimaryButton>
+                                        <span className="text-base font-semibold">
+                                            Sign Up
+                                        </span>
+                                    </PrimaryButton>
+                                </Link>
 
                                 <Link href={route('prototype.login')}>
                                     <PrimaryButton variant='light-outline'>
